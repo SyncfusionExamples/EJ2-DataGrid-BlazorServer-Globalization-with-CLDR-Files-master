352d64d424e69e64599ddc30076e40d03f2de14a
renjithsingh-rajendran123 <singhrenji@gmail.com>
2019-08-29 19:25:08 +0530

    added

--- SynfusionTest/Program.cs
+++ SynfusionTest/Program.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -27,33 +26,3 @@ namespace SynfusionTest
                 });
     }
 }
-=======
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-
-namespace SynfusionTest
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
->>>>>>> 5b01505b3cf3f309597c5a41fa105854eff3803a
